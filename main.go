b5a769cfe7da72b2393ee6cf3bae357f42777581
zhouyanggodking <zhou.yang.god.king@gmail.com>
2018-12-23 22:22:49 +0800

    package & function

--- main.go
+++ main.go
@@ -1,7 +1,21 @@
 package main
 
 import "fmt"
+import "go-tutorials/shape"
 
 func main() {
+	const a = 5
+	fmt.Println(a)
 	fmt.Printf("hello, world\n")
+	print(8, 9)
+
+	shape.Draw()
+}
+
+func print(num, t int) {
+	fmt.Println(num,  t)
+}
+
+func init() {
+	fmt.Println("init func in main package")
 }
---
+++ shape/rectangle.go
@@ -0,0 +1,11 @@
+package shape
+
+import "fmt"
+
+func Draw() {
+	fmt.Println("rectangle display")
+}
+
+func init() {
+	fmt.Println("shape init in rectangle")
+}
